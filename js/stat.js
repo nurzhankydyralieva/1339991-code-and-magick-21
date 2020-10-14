56b96ad8aae5ff64d4f60527cf94b5524fdf44b8
nurzhan kydyralieva <nurzhankydyralieva@gmail.com>
2020-10-14 18:25:10 +0600

    Учебный проект холст

--- js/stat.js
+++ js/stat.js
@@ -5,12 +5,12 @@ const CLOUD_HEIGTH = 270;
 const CLOUD_X = 100;
 const CLOUD_Y = 10;
 const GAP = 10;
-
-const GAP_Y = 150;
-const FONT_GAP = 15;
-const TEXT_WIDTH = 50;
-const BAR_HEIGHT = 20;
-const BAR_WIDTH = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+const GAP_X = 90;
+const GAP_Y = 260;
+const FONT_GAP = 60;
+const TEXT_WIDTH = 30;
+const BAR_WIDTH = 50;
+const BAR_HEIGTH = CLOUD_HEIGTH - GAP - GAP_X - GAP;
 
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -19,7 +19,6 @@ const renderCloud = function (ctx, x, y, color) {
 
 const getMaxElement = function (arr) {
   let maxElement = arr[0];
-
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
@@ -33,29 +32,28 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X, CLOUD_Y, `#fff`);
 
   ctx.fillStyle = `#000`;
-
   ctx.font = `16px PT Mono`;
-  ctx.fillText(`Ура вы победили!`, 115, 50);
-  ctx.fillText(`Список результатов:`, 115, 70);
-
+  ctx.fillText(`Ура вы победили!`, 140, 30);
+  ctx.fillText(`Список результатов:`, 140, 50);
   const maxTime = getMaxElement(times);
 
   for (let i = 0; i < players.length; i++) {
     ctx.fillText(
-      times[i],
-      CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i,
-      CLOUD_X
+      players[i],
+      CLOUD_X + GAP + TEXT_WIDTH + (GAP + GAP_X) * i,
+      CLOUD_Y + GAP_Y
     );
     ctx.fillText(
-      players[i],
-      CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i,
-      CLOUD_X + GAP + GAP_Y
+      times[i],
+      CLOUD_X + GAP + TEXT_WIDTH + (GAP + GAP_X) * i,
+      CLOUD_Y + FONT_GAP
     );
+
     ctx.fillRect(
-      CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i,
-      (BAR_HEIGHT * times[i]) / maxTime,
-      BAR_HEIGHT,
-      CLOUD_X + GAP + GAP_Y
+      CLOUD_X + GAP + TEXT_WIDTH + (GAP + GAP_X) * i,
+      CLOUD_Y + FONT_GAP + GAP,
+      BAR_WIDTH,
+      (BAR_HEIGTH * times[i]) / maxTime
     );
   }
 };
